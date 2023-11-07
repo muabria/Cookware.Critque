c1245b90bab2cc1b841a79bcff180e141a04c65a
Kat Christensen <katchristensen336@gmail.com>
2023-11-07 11:17:31 -0500

    Added api and auth routes

--- server/api/index.js
+++ server/api/index.js
@@ -2,18 +2,109 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const requireUser = require('../auth/utils');
+
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-//<------------------------GET ALL USERS------------------------>
-//GET /api/posts
-apiRouter.get("/", async (req, res, next) => {
+//<--------------------------------GET ALL EQUIPMENT-------------------------------->
+//GET /api/equipment
+apiRouter.get("/equipment", async (req, res, next) => {
     try {
-        const posts = await prisma.post.findMany();
-        res.send(posts);
+        const equipment = await prisma.equipment.findMany();
+        res.send(equipment);
     } catch (error) {
         next(error);
     }
 });
+
+//<--------------------------------GET SINGLE EQUIPMENT-------------------------------->
+//GET /api/equipment/:id
+apiRouter.get("/equipment/:id", async (req, res, next) => {
+    try {
+        const equipment = await prisma.equipment.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.send(equipment);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//<--------------------------------GET ALL REVIEWS-------------------------------->
+//GET /api/review
+apiRouter.get("/review", async (req, res, next) => {
+    try {
+        const review = await prisma.post.findMany();
+        res.status(200).res.send(review);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//<--------------------------------GET SINGLE REVIEW-------------------------------->
+//GET /api/review/:id
+apiRouter.get("/review/:id", async (req, res, next) => {
+    try {
+        const review = await prisma.post.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.status(200).res.send(review);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//<--------------------------------MAKE NEW REVIEW-------------------------------->
+//POST /api/review
+//NOTE: Need to have requireUser added. 
+//Prisma error will be: arguement user is missing
+apiRouter.post("/review", async(req, res, next) => {
+    try{
+const { title, content, rating, equipment } = req.body;
+const newReview = await prisma.post.create({
+    data: {
+        title,
+        content, 
+        rating, 
+        equipment,
+    }
+})
+res.status(200).res.send(newReview)
+    }catch (error){
+        next(error)
+    }
+});
+
+//<--------------------------------UPDATE REVIEW-------------------------------->
+//PATCH /api/review/:id
+//NOTE: Need to have requireUser added
+apiRouter.patch("/review/:id", async (req, res, next) => {
+   try {
+    const { title, content, rating, equipment } = req.body;
+    const updatedReview = await prisma.post.update({
+        where: {
+            id: Number(req.params.id)
+        },
+        data: {
+            title, 
+            content, 
+            rating,
+            equipment
+        }
+    })
+    res.status(200).res.send(updatedReview)
+   } catch(error){
+    next(error);
+   }
+});
+
+//<--------------------------------DELETE REVIEW-------------------------------->
+//NOTE: FOR INDIVIDUAL USER AND ADMIN
+//DELETE /api/review/:id
 
 module.exports = apiRouter;
--- server/auth/index.js
+++ server/auth/index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const authRouter = express.Router();
 
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = process.env
+
 const { requireUser } = require('./utils');
 
 const { PrismaClient } = require("@prisma/client");
@@ -10,15 +13,60 @@ const prisma = new PrismaClient();
 const bcrypt = require("bcrypt");
 const SALT_COUNT = 10;
 
-//<------------------------GET ALL USERS------------------------>
+//<--------------------------------GET ALL USERS-------------------------------->
 //GET /auth/users
-authRouter.get("/", async (req, res, next) => {
+authRouter.get("/user", async (req, res, next) => {
     try {
         const users = await prisma.user.findMany();
-        res.send(users);
+        res.status(200).res.send(users);
     } catch (error) {
         next(error);
     }
 });
+//<--------------------------------REGISTER USER-------------------------------->
+// POST /auth/signup
+authRouter.post("/signup", async (req, res, next) => {
+    try {
+        const { username, email, password, } = req.body
+        const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
 
+        const user = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword
+            }
+        });
+        delete user.password
+        const token = jwt.sign({ id: user.id, username }, process.env.JWT_SECRET);
+        res.status(200).send({ user, token });
+    } catch (error) {
+        next(error)
+    }
+})
+
+//<--------------------------------LOGIN USERS-------------------------------->
+//POST /auth/login
+authRouter.post("/login", async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        });
+        delete user.password
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+        res.send({ user, token })
+    } catch (error) {
+        next(error)
+    }
+});
+
+//<--------------------------------GET USER PROFILE-------------------------------->
+//GET /auth/user/:id
+
+//<--------------------------------DELETE USER-------------------------------->
+//NOTE: ONLY FOR ADMIN
+//DELETE /auth/user/:id
 module.exports = authRouter;
