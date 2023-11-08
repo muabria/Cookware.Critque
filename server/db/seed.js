e5a94c64c5867ee32444c749787ee1810ff23975
Marisa <marisavandellos@gmail.com>
2023-11-07 16:09:59 -0800

    added user seed, mostly

--- server/db/seed.js
+++ server/db/seed.js
@@ -18,21 +18,103 @@ async function seed() {
     console.log("Seeding the database.");
     try {
         //User entries
+        const marisa = await prisma.user.upsert({
+            where: {username: 'mvandell'},
+            update: {},
+            create: {
+                username: 'mvandell',
+                email: 'marisavandellos@gmail.com',
+                password: mvPass.toString(),
+                posts: {
+                    create: {
 
+                    }
+                },
+                isAdmin: true,
+                comments: {
+                    create: {
 
-        //Post entries
+                    }
+                }
+            }
+        })
 
+        const brianna = await prisma.user.upsert({
+            where: {username: 'muabria'},
+            update: {},
+            create: {
+                username: 'muabria',
+                email: '',
+                password: bmPass.toString(),
+                posts: {
+                    create: {
+
+                    }
+                },
+                isAdmin: true,
+                comments: {
+                    create: {
+                        
+                    }
+                }
+            }
+        })
+
+        const henrietta = await prisma.user.upsert({
+            where: {username: 'mizhenn'},
+            update: {},
+            create: {
+                username: 'mizhenn',
+                email: '',
+                password: hmPass.toString(),
+                posts: {
+                    create: {
+
+                    }
+                },
+                isAdmin: true,
+                comments: {
+                    create: {
+                        
+                    }
+                }
+            }
+        })
+
+        const kat = await prisma.user.upsert({
+            where: {username: 'katc'},
+            update: {},
+            create: {
+                username: 'katc',
+                email: '',
+                password: kcPass.toString(),
+                posts: {
+                    create: {
+
+                    }
+                },
+                isAdmin: true,
+                comments: {
+                    create: {
+                        
+                    }
+                }
+            }
+        })
 
         //Equipment entries
+        
 
-
-        //Comment entries
-
-
+        
         //Category entries
 
 
         
+        //Post entries
+        //Comment entries
+
+
+
         delete mvPass, bmPass, hmPass, kcPass;
         console.log("Database is seeded.")
     }
