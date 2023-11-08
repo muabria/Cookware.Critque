5613f6b075241c5ef267315fc8f4589858843857
Marisa <marisavandellos@gmail.com>
2023-11-07 17:56:55 -0800

    added entries for a few more tables

--- server/db/seed.js
+++ server/db/seed.js
@@ -103,12 +103,111 @@ async function seed() {
         })
 
         //Equipment entries
+        const trivet = await prisma.equipment.upsert({
+            where: {name: "Cork Trivet"},
+            update: {},
+            create: {
+                name: "Cork Trivet",
+                description: "A 7-inch circle of cork that protects counter and other surfaces from hot, cold, and wet items. Ideal for hot pans and water stains.",
+                image: "https://m.media-amazon.com/images/I/41TDVYs3YsL._AC_US100_.jpg",
+                posts: {
+                    create: {
 
+                    }
+                },
+                categoryId: 1,
+                brand: "IKEA",
+                purchaseLink: "https://www.amazon.com/dp/B00QOLXPV0?psc=1&ref=ppx_yo2ov_dt_b_product_details",
+                priceRating: 1
+            }
+        })
 
+        const dobie = await prisma.equipment.upsert({
+            where: {name: "Dobie"},
+            update: {},
+            create: {
+                name: "Dobie",
+                description: "A cleaning sponge with a little abrasion to get food off.",
+                image: "https://m.media-amazon.com/images/I/71WMDYKl1PL._AC_SX679_.jpg",
+                posts: {
+                    create: {
+
+                    }
+                },
+                categoryId: 3,
+                brand: "Scotch-Brite",
+                purchaseLink: "https://www.amazon.com/Scotch-Brite-Dobie-All-Purpose-Pad-Count/dp/B0011DR9WC/ref=sr_1_7?crid=17V6Q2H276G01&keywords=dobie&qid=1699408365&s=home-garden&sprefix=dobie%2Cgarden%2C152&sr=1-7",
+                priceRating: 2
+            }
+        })
+
+        const item = await prisma.equipment.upsert({
+            where: {name: ""},
+            update: {},
+            create: {
+                name: "",
+                description: "",
+                image: "",
+                posts: {
+                    create: {
+
+                    }
+                },
+                categoryId: 3,
+                brand: "",
+                purchaseLink: "",
+                priceRating: 2
+            }
+        })
         
         //Category entries
+        const basic = await prisma.category.upsert({
+            where: {category: "Basic"}, //id 1
+            update: {},
+            create: {
+                category: "Basic"
+            }
+        })
+        
+        const cooking = await prisma.category.upsert({
+            where: {category: "Cooking"}, //id 2
+            update: {},
+            create: {
+                category: "Cooking"
+            }
+        })
 
+        const cleaning = await prisma.category.upsert({
+            where: {category: "Cleaning"}, //id 3
+            update: {},
+            create: {
+                category: "Cleaning"
+            }
+        })
 
+        const storage = await prisma.category.upsert({
+            where: {category: "Storage"}, //id 4
+            update: {},
+            create: {
+                category: "Storage"
+            }
+        })
+
+        const gadgets = await prisma.category.upsert({
+            where: {category: "Gadgets"}, //id 5
+            update: {},
+            create: {
+                category: "Gadgets"
+            }
+        })
+
+        const miscellaneous = await prisma.category.upsert({
+            where: {category: "Miscellaneous"}, //id 6
+            update: {},
+            create: {
+                category: "Miscellaneous"
+            }
+        })
         
         //Post entries
         //Comment entries
