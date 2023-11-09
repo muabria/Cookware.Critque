fc1354c99e1de469f89656fa92f92d7f384cfc91
Marisa <marisavandellos@gmail.com>
2023-11-08 16:49:49 -0800

    seed works and has entries

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,5 +1,5 @@
 // TODO: seed the DB using Prisma
-const db = require("../db");
+const prisma = require("./client");
 const bcrypt = require("bcrypt");
 const SALT_COUNT = 10;
 
@@ -16,208 +16,153 @@ const kcPass = hash("De$ignCentra1");
 
 async function seed() {
     console.log("Seeding the database.");
+    await prisma.comment.deleteMany();
+    await prisma.post.deleteMany();
+    await prisma.user.deleteMany();
+    await prisma.equipment.deleteMany();
+    await prisma.category.deleteMany();
+
     try {
-        //User entries
-        const marisa = await prisma.user.upsert({
-            where: {username: 'mvandell'},
-            update: {},
-            create: {
-                username: 'mvandell',
-                email: 'marisavandellos@gmail.com',
-                password: mvPass.toString(),
-                posts: {
-                    create: {
-
-                    }
-                },
-                isAdmin: true,
-                comments: {
-                    create: {
-
-                    }
-                }
-            }
-        })
-
-        const brianna = await prisma.user.upsert({
-            where: {username: 'muabria'},
-            update: {},
-            create: {
-                username: 'muabria',
-                email: '',
-                password: bmPass.toString(),
-                posts: {
-                    create: {
-
-                    }
-                },
-                isAdmin: true,
-                comments: {
-                    create: {
-                        
-                    }
-                }
-            }
-        })
-
-        const henrietta = await prisma.user.upsert({
-            where: {username: 'mizhenn'},
-            update: {},
-            create: {
-                username: 'mizhenn',
-                email: '',
-                password: hmPass.toString(),
-                posts: {
-                    create: {
-
-                    }
-                },
-                isAdmin: true,
-                comments: {
-                    create: {
-                        
-                    }
-                }
-            }
-        })
-
-        const kat = await prisma.user.upsert({
-            where: {username: 'katc'},
-            update: {},
-            create: {
-                username: 'katc',
-                email: '',
-                password: kcPass.toString(),
-                posts: {
-                    create: {
-
-                    }
-                },
-                isAdmin: true,
-                comments: {
-                    create: {
-                        
-                    }
-                }
-            }
-        })
-
-        //Equipment entries
-        const trivet = await prisma.equipment.upsert({
-            where: {name: "Cork Trivet"},
-            update: {},
-            create: {
-                name: "Cork Trivet",
-                description: "A 7-inch circle of cork that protects counter and other surfaces from hot, cold, and wet items. Ideal for hot pans and water stains.",
-                image: "https://m.media-amazon.com/images/I/41TDVYs3YsL._AC_US100_.jpg",
-                posts: {
-                    create: {
-
-                    }
-                },
-                categoryId: 1,
-                brand: "IKEA",
-                purchaseLink: "https://www.amazon.com/dp/B00QOLXPV0?psc=1&ref=ppx_yo2ov_dt_b_product_details",
-                priceRating: 1
-            }
-        })
-
-        const dobie = await prisma.equipment.upsert({
-            where: {name: "Dobie"},
-            update: {},
-            create: {
-                name: "Dobie",
-                description: "A cleaning sponge with a little abrasion to get food off.",
-                image: "https://m.media-amazon.com/images/I/71WMDYKl1PL._AC_SX679_.jpg",
-                posts: {
-                    create: {
-
-                    }
-                },
-                categoryId: 3,
-                brand: "Scotch-Brite",
-                purchaseLink: "https://www.amazon.com/Scotch-Brite-Dobie-All-Purpose-Pad-Count/dp/B0011DR9WC/ref=sr_1_7?crid=17V6Q2H276G01&keywords=dobie&qid=1699408365&s=home-garden&sprefix=dobie%2Cgarden%2C152&sr=1-7",
-                priceRating: 2
-            }
-        })
-
-        const item = await prisma.equipment.upsert({
-            where: {name: ""},
-            update: {},
-            create: {
-                name: "",
-                description: "",
-                image: "",
-                posts: {
-                    create: {
-
-                    }
-                },
-                categoryId: 3,
-                brand: "",
-                purchaseLink: "",
-                priceRating: 2
-            }
-        })
-        
         //Category entries
-        const basic = await prisma.category.upsert({
-            where: {category: "Basic"}, //id 1
-            update: {},
-            create: {
+        const basic = await prisma.category.create({
+            data: {
                 category: "Basic"
             }
         })
-        
-        const cooking = await prisma.category.upsert({
-            where: {category: "Cooking"}, //id 2
-            update: {},
-            create: {
+
+        const cooking = await prisma.category.create({
+            data: {
                 category: "Cooking"
             }
         })
 
-        const cleaning = await prisma.category.upsert({
-            where: {category: "Cleaning"}, //id 3
-            update: {},
-            create: {
+        const cleaning = await prisma.category.create({
+            data: {
                 category: "Cleaning"
             }
         })
 
-        const storage = await prisma.category.upsert({
-            where: {category: "Storage"}, //id 4
-            update: {},
-            create: {
+        const storage = await prisma.category.create({
+            data: {
                 category: "Storage"
             }
         })
 
-        const gadgets = await prisma.category.upsert({
-            where: {category: "Gadgets"}, //id 5
-            update: {},
-            create: {
+        const gadgets = await prisma.category.create({
+            data: {
                 category: "Gadgets"
             }
         })
 
-        const miscellaneous = await prisma.category.upsert({
-            where: {category: "Miscellaneous"}, //id 6
-            update: {},
-            create: {
+        const miscellaneous = await prisma.category.create({
+            data: {
                 category: "Miscellaneous"
             }
         })
-        
+
+        //Equipment entries
+        const trivet = await prisma.equipment.create({
+            data: {
+                name: "Cork Trivet",
+                description: "A 7-inch circle of cork that protects counter and other surfaces from hot, cold, and wet items. Ideal for hot pans and water stains.",
+                image: "https://m.media-amazon.com/images/I/41TDVYs3YsL._AC_US100_.jpg",
+                category: { connect: { id: basic.id } },
+                brand: "IKEA",
+                purchaseLink: "https://www.amazon.com/dp/B00QOLXPV0?psc=1&ref=ppx_yo2ov_dt_b_product_details",
+                priceRating: 1
+            },
+            include: { category: true }
+        })
+
+        const dobie = await prisma.equipment.create({
+            data: {
+                name: "Dobie",
+                description: "A cleaning sponge with a little abrasion to get food off.",
+                image: "https://m.media-amazon.com/images/I/71WMDYKl1PL._AC_SX679_.jpg",
+                category: { connect: { id: cleaning.id } },
+                brand: "Scotch-Brite",
+                purchaseLink: "https://www.amazon.com/Scotch-Brite-Dobie-All-Purpose-Pad-Count/dp/B0011DR9WC/ref=sr_1_7?crid=17V6Q2H276G01&keywords=dobie&qid=1699408365&s=home-garden&sprefix=dobie%2Cgarden%2C152&sr=1-7",
+                priceRating: 2
+            },
+            include: { category: true }
+        })
+
+        //User entries
+        const marisa = await prisma.user.create({
+            data: {
+                username: 'mvandell',
+                email: 'marisavandellos@gmail.com',
+                password: mvPass.toString(),
+                isAdmin: true,
+            },
+
+        })
+
+        const brianna = await prisma.user.create({
+            data: {
+                username: 'muabria',
+                email: 'email@email.com',
+                password: bmPass.toString(),
+                isAdmin: true,
+            },
+
+        })
+
+        const henrietta = await prisma.user.create({
+            data: {
+                username: 'mizhenn',
+                email: 'email@email.com',
+                password: hmPass.toString(),
+                isAdmin: true,
+            },
+
+        })
+
+        const kat = await prisma.user.create({
+            data: {
+                username: 'katc333',
+                email: 'email@email.com',
+                password: kcPass.toString(),
+                isAdmin: true,
+            },
+
+        })
+
+
+
         //Post entries
         //Comment entries
+        const post1 = await prisma.post.create({
+            data: {
+                title: "Works great!",
+                content: 'I love this! I put one under everything',
+                rating: 5,
+                user: { connect: { id: marisa.id } },
+                equipment: { connect: { id: trivet.id } },
+                comments: {
+                    create: {
+                        content: 'Keeps my counter from getting water stains from my water pitcher',
+                        user: { connect: { id: marisa.id } },
+                    }
+                }
+            },
+            include: { user: true, equipment: true }
+        })
 
 
 
         delete mvPass, bmPass, hmPass, kcPass;
         console.log("Database is seeded.")
     }
-     catch(error) {
+    catch (error) {
         console.error(error);
     }
 }
+
+seed().then(async () => {
+    await prisma.$disconnect();
+}).catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect();
+    process.exit(1)
+})
