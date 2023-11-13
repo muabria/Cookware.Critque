66da3f4c281d175e57ace188f91d60d31bd962f0
mizhenn <128438195+mizhenn@users.noreply.github.com>
2023-11-13 18:51:36 -0500

    added authslice

---
+++ client/redux/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import api from './api';
+
+
+// Create a Redux slice for authentication
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {token: null, user: null},
+    reducers: {
+        logout: (_state) => ({token: null, user: null})
+    },
+
+    },
+       // Handle the 'register' API call success
+    extraReducers: (builder) => {
+        builder.addMatcher(
+            api.endpoints.register.matchFulfilled,
+            (state, {payload}) => ({token: payload.token, user:payload.user})
+        );
+
+        // Handle the 'login' API call success
+        builder.addMatcher(
+            api.endpoints.login.matchFulfilled,
+            (state, {payload}) => ({token: payload.token, user:payload.user})
+        );
+
+    }
+);
+
+export default authSlice.reducer;
+
+export const {logout} = authSlice.actions;
