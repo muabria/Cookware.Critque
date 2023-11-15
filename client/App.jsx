c1910e93b25ba00de447a30cc681d71fcabc041a
Kat Christensen <katchristensen336@gmail.com>
2023-11-15 13:59:25 -0500

    Reconfigure routers

--- client/App.jsx
+++ client/App.jsx
@@ -1,16 +1,21 @@
-import RegisterForm from "./components/RegisterForm";
-import LoginForm from "./components/LoginForm";
-import NavBar from "./components/NavBar";
-
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import AppContents from "./AppContents";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./components/HomePage";
 
 const App = () => {
 
   return (
     <Provider store={store}>
-      <AppContents />
+      <BrowserRouter>
+        <AppContents />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
