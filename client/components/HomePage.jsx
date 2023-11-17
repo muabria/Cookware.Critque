5d2428ac3747843172efea83db9be9730a76eb92
Kat Christensen <katchristensen336@gmail.com>
2023-11-17 17:33:15 -0500

    Adding comment form

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -2,6 +2,7 @@ import LoginForm from "./AuthorizationForms/LoginForm"
 import NewPostForm from "./AddNewPost/NewPostForm"
 import SearchCard from "./SearchEquipment/SearchCard"
 import AllPost from "../AllPosts"
+import CommentForm from "./commentForm"
 
 const HomePage = () => {
     return (
