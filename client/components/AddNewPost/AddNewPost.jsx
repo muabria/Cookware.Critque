44eff5a885e68c4a62953f8918219b4766f0dfae
Kat Christensen <katchristensen336@gmail.com>
2023-11-22 14:52:42 -0500

    Update make a new post form

--- client/components/AddNewPost/AddNewPost.jsx
+++ client/components/AddNewPost/AddNewPost.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { usePostReviewMutation } from "../../redux/api";
 import { useGetEquipmentQuery } from "../../redux/api";
 
+import { motion, useAnimationControls } from "framer-motion";
+
+import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button"
 import Card from "@mui/material/Card"
 import Grid from "@mui/material/Grid"
@@ -12,6 +15,7 @@ import Stack from "@mui/material/Stack";
 import Rating from "@mui/material/Rating";
 import Box from '@mui/material/Box';
 import StarIcon from '@mui/icons-material/Star';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const AddPostContent = () => {
 
@@ -34,109 +38,157 @@ const AddPostContent = () => {
             console.error(error)
         }
     }
+    //<----------------------ANIMATIONS---------------------->
+    const controls = useAnimationControls();
+    const variants = {
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 },
+    }
+
+    const handelAnimation = async (event) => {
+        controls.start("visible")
+    }
     //<-----------------STAR RATING STATE ------------------->
     const [hover, setHover] = useState(-1);
 
     function getLabelText(value) {
-        return `${rating} Star${rating !== 1 ? 's' : ''}, ${labels[value]}`;
+        return `${rating} Star${rating !== 1 ? 's' : ''}, ${starLabels[value]}`;
     }
-
-    const labels = {
+    const starLabels = {
         1: `Useless: Just doesn't cut it`,
         2: `Poor: Not exactly what I whisked for`,
         3: `Ok: Decent when everything else is dirty`,
         4: `Good: A-peeling and good in a kitchen`,
         5: `Excellent: Truely egg-ceptional!`,
     };
-
+    console.log(equipment);
     return (
         <>
             <Grid container>
-                <Grid item xs={3}>
+                <Grid item xs={2}>
 
                 </Grid>
 
-                <Grid item xs={6}>
-                <Box sx={{ backgroundColor: "#89c7c3" }}>
-                            <Typography variant="h4" sx={{ textAlign: "center", p: 1 }}>
-                                Make a Kitchen Equipment Critique:
-                            </Typography>
-                        </Box>
-                    <Card sx={{ backgroundColor: "#D9E4DD", p: 5 }}>
-    
-                        <form onSubmit={handleSubmit}>
+                <Grid item xs={8}>
+                    <Box sx={{ backgroundColor: "#89c7c3" }}>
+                        <Typography variant="h4" sx={{ textAlign: "center", p: 1 }}>
+                            Make a Kitchen Equipment Critique:
+                        </Typography>
+                    </Box>
 
-                            <Stack direction="column">
-                                {/* <---------------------------SELECT EQUIPMENT-----------------------------> */}
+                    <form onSubmit={handleSubmit}>
+                        <Stack direction="column">
+                            {/* <-----------------SELECT EQUIPMENT-------------------> */}
+                            <Card sx={{ backgroundColor: "#D9E4DD", p: 5, mb: 3 }}>
+                                <Stack direction="row">
+                                    <Avatar sx={{ color: "#3C1B1F", backgroundColor: "#E7B10A", border: "solid #D29D2B 5px" }}>
+                                        1
+                                    </Avatar>
+                                    <Typography variant="h5" sx={{ p: 1 }}>
+                                        Click on the Equipment You're Reviewing:
+                                    </Typography>
+                                </Stack>
+                                {/* <--------------MAPP EQUIPMENT---------------> */}
                                 <Stack direction="row">
                                     {data && data.map((equipment) => (
-                                        <Card key={equipment.id}
-                                            sx={{  m: 1, p: 1, maxWidth: 200 }}>
-                                            <Typography variant="h6" sx={{ m: 1 }}>
-                                                {equipment.name}
-                                            </Typography>
-                                            <img
-                                                src={equipment.image}
-                                                alt={`${equipment.name} image`}
-                                                width="50"
-                                                height="50" />
-                                            <Button
-                                                onClick={() => setEquipment(equipment.id)}
-                                                sx={{ m: 1 }}>
-                                                Select
-                                            </Button>
+                                        <Card
+                                            key={equipment.id}
+                                            className="select-equipment"
+                                            onClick={() => setEquipment(equipment.id)}
+                                            sx={{ m: 1, minWidth: 300, maxWidth: 300 }}>
+                                            <Box sx={{ backgroundColor: "#EACD65" }}>
+                                                <Typography variant="h5" sx={{ m: 1, textAlign: "center" }}>
+                                                    {equipment.name}
+                                                </Typography>
+                                                <Typography sx={{ m: 1, textAlign: "center" }}>
+                                                    by {equipment.brand}
+                                                </Typography>
+                                            </Box>
+                                            <Box textAlign="center">
+                                                <img
+                                                    src={equipment.image}
+                                                    alt={`${equipment.name} image`}
+                                                    width="200"
+                                                    height="200" />
+                                            </Box>
                                         </Card>
                                     ))
                                     }
                                 </Stack>
-                                {/* <---------------------------TITLE TEXTFIELD-----------------------------> */}
-                                <TextField
-                                    label="Title"
-                                    value={title}
-                                    onChange={(event) => setTitle(event.target.value)}
-                                    size="small"
-                                    sx={{ m: 1, backgroundColor: "white" }}
-                                />
-                                {/* <---------------------------CONTENT TEXTFIELD-----------------------------> */}
-                                <TextField
-                                    label="Content"
-                                    value={content}
-                                    onChange={(event) => setContent(event.target.value)}
-                                    size="small"
-                                    sx={{ m: 1, backgroundColor: "white" }}
-                                    multiline
-                                />
-                                {/* <---------------------------RATING STARS-----------------------------> */}
-                                <Typography>
-                                    Rate this Equipment:
-                                </Typography>
-                                <Stack direction="row">
-                                    <Rating
-                                        name="Equipment Rating"
-                                        value={rating}
-                                        precision={1}
-                                        getLabelText={getLabelText}
-                                        onChange={(event) => setRating(event.target.value)}
-                                        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-                                    />
-                                    {/* <---------------------------RENDED RATING TEXT-----------------------------> */}
-                                    {rating !== null && (
-                                        <Box sx={{ mx: 2 }}>
-                                            <Typography>
-                                                {labels[hover !== -1 ? hover : rating]}
-                                            </Typography>
-                                        </Box>
-                                    )}
-                                </Stack>
-                                {/* <---------------------------SUBMIT BUTTON-----------------------------> */}
-                                <Button type="submit" sx={{ backgroundColor: "#088395", color: "white", m: 2, p: 1 }}>
-                                    Create Your Critique!
+                                <Button
+                                    onClick={() => handelAnimation()}
+                                    sx={{ backgroundColor: "#088395", color: "white", m: 2, p: 1 }}>
+                                    Next <ArrowForwardIosIcon /><ArrowForwardIosIcon /><ArrowForwardIosIcon />
                                 </Button>
-                            </Stack>
-                        </form>
-                    </Card>
+                            </Card>
+
+
+                            {/* <---------------------------SECOND CARD-----------------------------> */}
+
+                            <motion.div
+                                variants={variants}
+                                initial="hidden"
+                                animate={controls}>
+                                <Card sx={{ backgroundColor: "#D9E4DD", p: 5 }}>
+                                    <Stack direction="row">
+                                        <Avatar sx={{ color: "#3C1B1F", backgroundColor: "#E7B10A", border: "solid #D29D2B 5px" }}>
+                                            2
+                                        </Avatar>
+                                        <Typography variant="h5" sx={{ p: 1 }}>
+                                            Write Your Critique:
+                                        </Typography>
+                                    </Stack>
+                                    {/* <-----------------TITLE TEXTFIELD------------------> */}
+                                    <Stack direction="column">
+                                        <TextField
+                                            label="Add Your Critique's Title"
+                                            value={title}
+                                            onChange={(event) => setTitle(event.target.value)}
+                                            size="small"
+                                            sx={{ m: 1, backgroundColor: "white" }}
+                                        />
+                                        {/* <-----------------CONTENT TEXTFIELD-------------------> */}
+                                        <TextField
+                                            label="Add Your Critique's Content"
+                                            value={content}
+                                            onChange={(event) => setContent(event.target.value)}
+                                            size="small"
+                                            sx={{ m: 1, backgroundColor: "white" }}
+                                            multiline
+                                        />
+                                        {/* <-----------------RATING STARS-------------------> */}
+                                        <Typography>
+                                            Add Your Rating for this Equipment:
+                                        </Typography>
+                                        <Stack direction="row">
+                                            <Rating
+                                                name="Equipment Rating"
+                                                value={rating}
+                                                precision={1}
+                                                getLabelText={getLabelText}
+                                                onChange={(event) => setRating(event.target.value)}
+                                                emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                                            />
+                                            {/* <-----------------RENDED RATING TEXT-------------------> */}
+                                            {rating !== null && (
+                                                <Box sx={{ mx: 2 }}>
+                                                    <Typography> {starLabels[hover !== -1 ? hover : rating]} </Typography>
+                                                </Box>
+                                            )}
+                                        </Stack>
+                                    </Stack>
+                                    <Button type="submit" sx={{ backgroundColor: "#088395", color: "white", m: 2, p: 1 }}>
+                                        Create Your Critique!
+                                    </Button>
+                                </Card>
+                            </motion.div>
+
+                            {/* <-----------------SUBMIT BUTTON-------------------> */}
+                        </Stack>
+                    </form>
+
                 </Grid>
-                <Grid item xs={3}>
+                <Grid item xs={2}>
 
                 </Grid>
             </Grid>
