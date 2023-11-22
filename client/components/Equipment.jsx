af59fd9ee50018e82dab53a7b764b852e8057cc3
briaar <briarusss@gmail.com>
2023-11-22 18:46:26 -0500

    added more entries, still adding more..pushing for now

--- client/components/Equipment.jsx
+++ client/components/Equipment.jsx
@@ -1,5 +1,6 @@
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
 import { useGetEquipmentQuery } from "../redux/api"
 
@@ -14,10 +15,19 @@ const Equipment = () => {
         <>
             {data && data.map((equipment) => (
                 <Card key={equipment.id}>
+                    <Stack direction="column"> 
                     <Typography>
                         {equipment.name}
                     </Typography>
+                    <Typography>
+                        {equipment.brand}
+                    </Typography>
+                    <Typography>
+                        {equipment.description}
+                    </Typography>
+                    </Stack>
                 </Card>
+
             ))
 
             }
