cd6d80839fe1ae6e28c7a79750e8a38b7d4988aa
briaar <briarusss@gmail.com>
2023-11-29 19:27:44 -0500

    seed entry

--- server/db/seed.js
+++ server/db/seed.js
@@ -152,7 +152,7 @@ async function seed() {
                 description: "A juicer is a great tool for squeezing all the basics like lemon, lime, and orange juice when called for in multiple recipes – but you should take advantage of this appliance for juicing all varieties of fruits and vegetables. Juicers save you significant muscle and time by extracting the most juice possible while separating the parts of the produce you may not need or want – like pulp, skins, and seeds. These can go right to your compost bin!.",
                 image: "https://surlatable.aiy7.net/6eA6mq",
                 category: { connect: { id: gadgets.id } },
-                brand: "Ninja",
+                brand: "Ninja, Target, IKEA, Walmart",
                 purchaseLink: "",
                 priceRating: 2
             },
@@ -165,14 +165,53 @@ async function seed() {
                 description: "If you cannot renovate, simply add baskets to the shelves so that you can pull them out to access what is in the back. You can also Purcahse Pantry storage based containers to use as storage for you.",
                 image: "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_730,h_487/project%20prism%2Fcolor%20search%20archive%2Fa54c4855e6b1c7a66c738fd7be85d7a7b1cb503d",
                 category: { connect: { id: storage.id } },
-                brand: "",
+                brand: "Target, IKEA, Walmart",
                 purchaseLink: "",
                 priceRating: 2
             },
             include: { category: true }
         })
 
-        // const test = await prisma.equipment.create({
+        const parchmentPaperLiners = await prisma.equipment.create({
+            data: {
+                name: "Parchment Paper Liners",
+                description: "pre-cut 7 inch round air fryer parchment paper liners are suitable for most air fryers on the market and can be used for baking, frying, grilling and so on. ",
+                image: "https://m.media-amazon.com/images/I/719evGmaCGL._AC_SL1500_.jpg",
+                category: { connect: { id: cooking.id } },
+                brand: "Numola",
+                purchaseLink: "https://www.amazon.com/Numola-Perforated-Parchment-Compatible-3-4-3-7qt/dp/B08HYSXT9G?pd_rd_w=SI5fH&content-id=amzn1.sym.d1bf6bee-b588-4538-8422-ca1ad75c6390&pf_rd_p=d1bf6bee-b588-4538-8422-ca1ad75c6390&pf_rd_r=SMK5ST66K45MF0R6G5G1&pd_rd_wg=nGRZk&pd_rd_r=3f8cf1da-807d-4631-b3a4-df7cfa6cc4db&pd_rd_i=B08HYSXT9G&ref_=pd_bap_d_grid_rp_0_30_t&th=1",
+                priceRating: 1
+            },
+            include: { category: true }
+        })
+
+        const pastaServer = await prisma.equipment.create({
+            data: {
+                name: "Curve Handle Pasta Server",
+                description: "Tools are designed to lie flat on the countertop and will not roll, Nylon heads on tools are heat resistant up to 400-Degree Fahrenheit",
+                image: "https://m.media-amazon.com/images/I/61GOqhhQiyL._AC_SL1500_.jpg",
+                category: { connect: { id: miscellaneous.id } },
+                brand: "Amazon",
+                purchaseLink: "https://www.amazon.com/Cuisinart-CTG-01-PS-Curve-Handle-Server/dp/B004YZEIMS/ref=sr_1_1?crid=3AI2JR8NI1U3K&keywords=pasta%2Bspoon&qid=1700679440&sprefix=pasta%2Bspoon%2Caps%2C133&sr=8-1&th=1",
+                priceRating: 2
+            },
+            include: { category: true }
+        })
+
+        const bonAmi = await prisma.equipment.create({
+            data: {
+                name: "Bon Ami, Powder Cleanser",
+                description: "Bon Ami - All Natural Powder Cleanser Kitchen & Bath",
+                image: "https://m.media-amazon.com/images/I/61Zmt9jQX+L._AC_SL1000_.jpg",
+                category: { connect: { id: cleaning.id } },
+                brand: "Ninja",
+                purchaseLink: "https://www.amazon.com/Bon-Ami-Natural-Cleanser-Kitchen/dp/B07VC36NMC/ref=sr_1_1?crid=3A6SDLV1RQVIX&keywords=bon+ami&qid=1700679177&sprefix=bon+ami%2Caps%2C164&sr=8-1",
+                priceRating: 2
+            },
+            include: { category: true }
+        })
+
+        // const test4 = await prisma.equipment.create({
         //     data: {
         //         name: "",
         //         description: "",
@@ -185,7 +224,7 @@ async function seed() {
         //     include: { category: true }
         // })
 
-        // const test2 = await prisma.equipment.create({
+        // const test5 = await prisma.equipment.create({
         //     data: {
         //         name: "",
         //         description: "",
@@ -275,6 +314,113 @@ async function seed() {
                     create: {
                         content: 'Keeps my counter from getting water stains from my water pitcher',
                         user: { connect: { id: kat.id } },
+                    }
+                }
+            },
+            include: { user: true, equipment: true }
+        })
+
+
+        const post3 = await prisma.post.create({
+            data: {
+                title: "Buy it right now!",
+                content: 'I love this! I put one under everything',
+                rating: 5,
+                user: { connect: { id: brianna.id } },
+                equipment: { connect: { id: juicer.id } },
+                comments: {
+                    create: {
+                        content: 'definitely buy!',
+                        user: { connect: { id: brianna.id } },
+                    }
+                }
+            },
+            include: { user: true, equipment: true }
+        })
+
+
+        const post4 = await prisma.post.create({
+            data: {
+                title: "Ahmazinggg!",
+                content: 'I love this! I put one under everything',
+                rating: 5,
+                user: { connect: { id: henrietta.id } },
+                equipment: { connect: { id: handBlender.id } },
+                comments: {
+                    create: {
+                        content: 'definitely buy!',
+                        user: { connect: { id: henrietta.id } },
+                    }
+                }
+            },
+            include: { user: true, equipment: true }
+        })
+
+
+
+        const post5 = await prisma.post.create({
+            data: {
+                title: "Love this!",
+                content: 'I use this so often',
+                rating: 5,
+                user: { connect: { id: henrietta.id } },
+                equipment: { connect: { id: foodProcessor.id } },
+                comments: {
+                    create: {
+                        content: 'Love using this product, definitely buy!',
+                        user: { connect: { id: henrietta.id } },
+                    }
+                }
+            },
+            include: { user: true, equipment: true }
+        })
+
+
+        const post6 = await prisma.post.create({
+            data: {
+                title: "Love this!",
+                content: 'I use this so often',
+                rating: 5,
+                user: { connect: { id: marisa.id } },
+                equipment: { connect: { id: blender.id } },
+                comments: {
+                    create: {
+                        content: 'definitely buy!',
+                        user: { connect: { id: marisa.id } },
+                    }
+                }
+            },
+            include: { user: true, equipment: true }
+        })
+
+        const post7 = await prisma.post.create({
+            data: {
+                title: "Love this!",
+                content: 'I use this so often',
+                rating: 5,
+                user: { connect: { id: kat.id } },
+                equipment: { connect: { id: Mandolin.id } },
+                comments: {
+                    create: {
+                        content: 'definitely buy!',
+                        user: { connect: { id: kat.id } },
+                    }
+                }
+            },
+            include: { user: true, equipment: true }
+        })
+
+        const post8 = await prisma.post.create({
+            data: {
+                title: "Love this!",
+                content: 'I use this so often',
+                rating: 5,
+                user: { connect: { id: brianna.id } },
+                equipment: { connect: { id: pantryContainer.id } },
+                comments: {
+                    create: {
+                        content: 'definitely buy!',
+                        user: { connect: { id: brianna.id } },
                     }
                 }
             },
