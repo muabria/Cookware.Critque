d3abfd753f093d04d9b9a84bc6b6d6286e024da0
Marisa <marisavandellos@gmail.com>
2023-12-02 14:45:18 -0800

    squashed the evil salt bug

--- server/auth/index.js
+++ server/auth/index.js
@@ -115,11 +115,15 @@ authRouter.delete("/user/:id", requireAdmin, async (req, res, next) => {
 })
 
 //<--------------------------------PATCH USER-------------------------------->
-//PATCH /auth/user/:id
+//PATCH /auth/account/:id/edit
 authRouter.patch("/account/:id/edit", requireUser, async (req, res, next) => {
     try {
         const {username, email, password} = req.body;
-        const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+        const hashedPassword = ""
+        if (password !== undefined) {
+            hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+            return hashedPassword;
+        }
         
         const updatedUser = await prisma.user.update({
             where: {id: req.user.id},
