961197b297ac89fea743cb79a8e56e0f8658657e
Kat Christensen <katchristensen336@gmail.com>
2023-12-09 10:49:09 -0500

    Replace carousel with slide show

--- client/components/Categories/CategoryPage.jsx
+++ client/components/Categories/CategoryPage.jsx
@@ -1,12 +1,17 @@
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid"
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import { useMediaQuery, useTheme } from "@mui/material";
 
 import { useGetEquipmentQuery } from "../../redux/api";
 import { useGetSingleCategoryQuery } from "../../redux/api";
 
+import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 
 import { motion } from 'framer-motion';
@@ -15,8 +20,11 @@ import { useParams } from "react-router";
 
 
 import LoadingMessage from "../ErrorMessages/LoadingMessage"
+import SlideShow from "../SlideShow";
 
 const CategoryPage = () => {
+    const [scrollItem, setscrollItem] = useState(0);
+    const ref = useRef();
 
     const { id } = useParams();
 
@@ -30,67 +38,88 @@ const CategoryPage = () => {
         return <div>Error 404: Data not found. Maybe it's hiding in the pantry...</div>
     }
     if (isLoading) {
-        return <div><LoadingMessage/></div>;
+        return <div><LoadingMessage /></div>;
     }
     if (error) {
         return <div>Sorry! Something went wrong loading the categories.</div>;
     }
 
+    const handleScroll = (movementAmount) => {
+        const newscrollItem = scrollItem + movementAmount;
+        setscrollItem(newscrollItem);
+        ref.current.scrollLeft = newscrollItem;
+    };
+
     return (
         <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5, ease: "easeIn" }}>
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeIn" }}>
             {isMobile ?
                 <div>
                     <Typography variant="h3" sx={{ color: "#205375", textAlign: "center", mb: 2 }}>
                         All {data.category} Kitchen Equipment
                     </Typography>
-                    <div className="carousel">
-                        <motion.div className="inner-carousel" drag="x" dragConstraints={{ right: 0, left: -300 }}>
-                            {equipmentData && equipmentData.filter((equipment) => equipment.categoryId === data.id).map((equipment) => (
-                                <Card key={equipment.id}
-                                    sx={{
-                                        backgroundColor: "#F9FBE7",
-                                        border: "solid #D29D2B 5px",
-                                        borderRadius: "10px",
-                                        minWidth: 220,
-                                        minHeight: 220,
-                                        maxWidth: 220,
-                                        maxHeight: 220,
-                                        m: 1,
-                                        p: 1,
-                                    }}>
-                                    <Stack direction="column">
-                                        <Typography sx={{ color: "#205375", fontSize: "12px", textAlign: "center" }}>
-                                            {equipment.name}
-                                        </Typography>
-                                        <Typography variant="h6" sx={{ color: "#205375", fontSize: "11px", textAlign: "center" }}>
-                                            {equipment.brand}
-                                        </Typography>
-                                        <img
-                                            src={equipment.image}
-                                            alt={`${equipment.name} image`}
-                                            width="100"
-                                            height="100"
-                                        />
-                                        <Link to={`/equipment/${equipment.id}`} >
-                                            <Button
-                                                sx={{
-                                                    mt: 2,
-                                                    ml: 3,
-                                                    boxShadow: 3,
-                                                    color: "#3C1B1F",
-                                                    backgroundColor: "#EACD65",
-                                                    border: "solid #D29D2B 2px"
-                                                }}>
-                                                See All Reviews 
-                                            </Button>
-                                        </Link>
-                                    </Stack>
-                                </Card>
-                            ))}
-                        </motion.div>
+                    <Button
+                        sx={{ ml: 3, mr: 10 }}
+                        onClick={() => handleScroll(-250)}>
+                        <ArrowCircleLeftIcon />
+                    </Button>
+                    <Button
+                        sx={{ mr: 3, ml: 10 }}
+                        onClick={() => handleScroll(250)}>
+                        <ArrowCircleRightIcon />
+                    </Button>
+
+                    <div
+                        ref={ref}
+                        style={{ overflowX: "scroll", scrollBehavior: "smooth" }}>
+                        <Box sx={{ width: "80px" }}>
+                            <Stack direction="row">
+                                {equipmentData && equipmentData.filter((equipment) => equipment.categoryId === data.id).map((equipment) => (
+                                    <Card key={equipment.id}
+                                        sx={{
+                                            backgroundColor: "#F9FBE7",
+                                            border: "solid #D29D2B 5px",
+                                            borderRadius: "10px",
+                                            minWidth: 200,
+                                            minHeight: 200,
+                                            maxWidth: 200,
+                                            maxHeight: 200,
+                                            m: 1,
+                                            p: 1,
+                                        }}>
+                                        <Stack direction="column">
+                                            <Typography sx={{ color: "#205375", fontSize: "12px", textAlign: "center" }}>
+                                                {equipment.name}
+                                            </Typography>
+                                            <Typography variant="h6" sx={{ color: "#205375", fontSize: "11px", textAlign: "center" }}>
+                                                {equipment.brand}
+                                            </Typography>
+                                            <img
+                                                src={equipment.image}
+                                                alt={`${equipment.name} image`}
+                                                width="100"
+                                                height="100"
+                                            />
+                                            <Link to={`/equipment/${equipment.id}`} >
+                                                <Button
+                                                    sx={{
+                                                        mt: 2,
+                                                        ml: 3,
+                                                        boxShadow: 3,
+                                                        color: "#3C1B1F",
+                                                        backgroundColor: "#EACD65",
+                                                        border: "solid #D29D2B 2px"
+                                                    }}>
+                                                    See All Reviews
+                                                </Button>
+                                            </Link>
+                                        </Stack>
+                                    </Card>
+                                ))}
+                            </Stack>
+                        </Box>
                     </div>
                 </div>
                 ://is NOT mobile...
@@ -98,50 +127,54 @@ const CategoryPage = () => {
                     <Typography variant="h3" sx={{ color: "#205375", textAlign: "center", mb: 2 }}>
                         All {data.category} Kitchen Equipment
                     </Typography>
-                    <div className="carousel">
-                        <motion.div className="inner-carousel" drag="x" dragConstraints={{ right: 0, left: -300 }}>
-                            {equipmentData && equipmentData.filter((equipment) => equipment.categoryId === data.id).map((equipment) => (
-                                <Card key={equipment.id}
-                                    sx={{
-                                        backgroundColor: "#F9FBE7",
-                                        border: "solid #D29D2B 5px",
-                                        borderRadius: "10px",
-                                        minWidth: 200,
-                                        minHeight: 200,
-                                        m: 5,
-                                        p: 5,
-                                    }}>
-                                    <Stack direction="column">
-                                        <Typography variant="h5" sx={{ color: "#205375", textAlign: "center", mb: 2 }}>
-                                            {equipment.name}
-                                        </Typography>
-                                        <Typography variant="h6" sx={{ color: "#205375", textAlign: "center", mb: 2 }}>
-                                            {equipment.brand}
-                                        </Typography>
-                                        <img
-                                            src={equipment.image}
-                                            alt={`${equipment.name} image`}
-                                            width="200"
-                                            height="200"
-                                        />
-                                        <Link to={`/equipment/${equipment.id}`} >
-                                            <Button
-                                                sx={{
-                                                    my: 2,
-                                                    ml: 8,
-                                                    boxShadow: 3,
-                                                    color: "#3C1B1F",
-                                                    backgroundColor: "#EACD65",
-                                                    border: "solid #D29D2B 2px"
-                                                }}>
-                                                See the {equipment.name}'s Reviews
-                                            </Button>
-                                        </Link>
-                                    </Stack>
-                                </Card>
-                            ))}
-                        </motion.div>
-                    </div>
+                    <SlideShow
+                        content={
+                            <>
+                                {equipmentData && equipmentData.filter((equipment) => equipment.categoryId === data.id).map((equipment) => (
+                                    <Card key={equipment.id}
+                                        sx={{
+                                            backgroundColor: "#F9FBE7",
+                                            border: "solid #D29D2B 5px",
+                                            borderRadius: "10px",
+                                            minWidth: 220,
+                                            minHeight: 220,
+                                            maxWidth: 220,
+                                            maxHeight: 220,
+                                            m: 1,
+                                            p: 1,
+                                        }}>
+                                        <Stack direction="column">
+                                            <Typography sx={{ color: "#205375", fontSize: "12px", textAlign: "center" }}>
+                                                {equipment.name}
+                                            </Typography>
+                                            <Typography variant="h6" sx={{ color: "#205375", fontSize: "11px", textAlign: "center" }}>
+                                                {equipment.brand}
+                                            </Typography>
+                                            <img
+                                                src={equipment.image}
+                                                alt={`${equipment.name} image`}
+                                                width="100"
+                                                height="100"
+                                            />
+                                            <Link to={`/equipment/${equipment.id}`} >
+                                                <Button
+                                                    sx={{
+                                                        mt: 2,
+                                                        ml: 3,
+                                                        boxShadow: 3,
+                                                        color: "#3C1B1F",
+                                                        backgroundColor: "#EACD65",
+                                                        border: "solid #D29D2B 2px"
+                                                    }}>
+                                                    See All Reviews
+                                                </Button>
+                                            </Link>
+                                        </Stack>
+                                    </Card>
+                                ))}
+                            </>
+                        }
+                    />
                 </div>}
         </motion.div>
     )
--- client/components/Posts/AddNewPost.jsx
+++ client/components/Posts/AddNewPost.jsx
@@ -23,6 +23,7 @@ import { useMediaQuery, useTheme } from "@mui/material";
 import MobileNewPost from "./MobileNewPost";
 import LoadingMessage from "../ErrorMessages/LoadingMessage";
 import AccountRedirect from "../ErrorMessages/AccountRedirect";
+import SlideShow from "../SlideShow";
 import { useNavigate } from "react-router-dom";
 
 const AddPostContent = () => {
@@ -57,11 +58,11 @@ const AddPostContent = () => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
-    if(!userData) {
-        return(<div><AccountRedirect/></div>)
+    if (!userData) {
+        return (<div><AccountRedirect /></div>)
     }
     if (isLoading) {
-        return (<div><LoadingMessage/></div>)
+        return (<div><LoadingMessage /></div>)
     }
     if (error) {
         return <div> Sorry! There's a problem loading the equipment. </div>
@@ -104,11 +105,11 @@ const AddPostContent = () => {
 
     return (
         <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5, ease: "easeIn" }}>
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeIn" }}>
             {isMobile ?
-                <div><MobileNewPost/></div>
+                <div><MobileNewPost /></div>
                 : //is NOT mobile...
                 <div>
                     <Grid container>
@@ -137,16 +138,16 @@ const AddPostContent = () => {
                                             </Typography>
                                         </Stack>
                                         {/* <--------------MAP EQUIPMENT---------------> */}
-                                        <div className="carousel">
-                                            <motion.div className="inner-carousel" drag="x" dragConstraints={{ right: 0, left: -2000 }}>
-                                                <Stack direction="row">
+                                        <SlideShow
+                                            content={
+                                                <>
                                                     {data && data.map((equipment) => (
                                                         <Card
                                                             key={equipment.id}
                                                             className="select-equipment"
                                                             onClick={() => setEquipment(equipment.id)}
-                                                            sx={{ m: 1, minWidth: 300, maxWidth: 300, border: "solid #D29D2B 5px", borderRadius: 100 }}>
-                                                            <Box sx={{ backgroundColor: "#EACD65" }}>
+                                                            sx={{ m: 1, minWidth: 350, maxWidth: 350, border: "solid #D29D2B 5px", borderRadius: 100 }}>
+                                                            <Box sx={{ pt: 5, px: 2, backgroundColor: "#EACD65" }}>
                                                                 <Typography
                                                                     variant="h5"
                                                                     sx={{ color: "#205375", m: 1, textAlign: "center" }}>
@@ -165,9 +166,9 @@ const AddPostContent = () => {
                                                             </Box>
                                                         </Card>
                                                     ))}
-                                                </Stack>
-                                            </motion.div>
-                                        </div>
+                                                </>
+                                            }
+                                        />
                                         <Button
                                             onClick={() => handelAnimation()}
                                             sx={{ backgroundColor: "#088395", color: "white", m: 2, p: 1 }}>
