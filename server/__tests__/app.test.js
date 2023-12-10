b644aaced208fcdb2f1ad01d66ded8dad4a45eaf
briaar <briarusss@gmail.com>
2023-12-10 13:04:33 -0500

    testing.stumped but written out

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -1,5 +1,7 @@
 const app = require('../app');
 const request = require('supertest');
+const prismaClient  = require('@prisma/client');
+
 
 /* NOTE: this is just an example test, you can delete it
 - your tests should be in folders __tests__ co-located with
@@ -10,8 +12,138 @@ For example: api/__tests__/users.test.js would test api/users.js
 
 describe('Express App', () => {
     it('is live and responding to requests', async () => {
-        const response = await request(app).get('/');
+        const response = await request(app).get('/api');
 
         expect(response.status).toBe(200);
     })
-})
+});
+
+
+// <-------- 'GET' EQUIPMENT TESTING --------> 
+// it('should return all equipment', async () => {
+//     const mockEquipment = [prisma];
+//     console.log(mockEquipment)
+//     prismaClient.equipment.findUnique();
+//     const response = await request(app).get('/api/equipment');
+//     expect(response.statusCode).toBe(200);
+//     expect(response.body).toEqual(mockEquipment);
+//   });
+  
+  // <-------- 'GET' SINGLE EQUIPMENT TESTING --------> 
+  
+  it('should return single equipment', async () => {
+    const id = '';
+    const mockEquipment = { id };
+    prismaClient.equipment.findUnique.mockReturnValue(mockEquipment);
+    const response = await request(app).get('/api/equipment/:id');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(mockEquipment);
+  });
+  
+  // <-------- 'POST' EQUIPMENT TESTING --------> 
+  it('should create new equipment', async () => {
+    const mockEquipment = { name: 'Test Equipment' };
+    prisma.equipment.create.mockReturnValue(mockEquipment);
+    const response = await request(app)
+      .post('/api/equipment')
+      .send({ name: 'Test Equipment' });
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual(mockEquipment);
+  });
+
+  
+  // <-------- 'PATCH' EQUIPMENT TESTING --------> 
+  it('should update existing equipment', async () => {
+    const mockEquipment = { id: 1, name: 'Updated Equipment' };
+    prisma.equipment.update.mockReturnValue(mockEquipment);
+    const response = await request(app)
+      .patch('/api/equipment/1')
+      .send({ name: 'Updated Equipment' });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(mockEquipment);
+  });
+  
+
+  // <------- 'DELETE' EQUIPMENT TESTING --------> 
+
+  it('should delete equipment', async () => {
+    const mockEquipment = { id: 1 };
+    prisma.equipment.delete.mockReturnValue(mockEquipment);
+    const response = await request(app).delete('/api/equipment/1');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(mockEquipment);
+  });
+  
+
+
+
+// <------------ END TO END TESTING -------------> 
+
+describe('authRouter end-to-end tests', () => {
+    let serverInstance;
+    let userToken;
+  
+    beforeAll(async () => {
+      // Start the server
+      serverInstance = await createServer();
+    });
+  
+    afterAll(async () => {
+      // Stop the server
+      await serverInstance.close();
+    });
+  
+    beforeEach(async () => {
+      // Clear existing user data
+      await prisma.user.deleteMany({});
+    });
+  
+    it('should successfully register a new user', async () => {
+      const username = 'johndoe';
+      const email = 'johndoe@example.com';
+      const password = 'password123';
+  
+      const response = await request(serverInstance)
+        .post('/auth/register')
+        .send({ username, email, password });
+  
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('user');
+      expect(response.body.user.username).toBe(username);
+      expect(response.body.user.email).toBe(email);
+      expect(response.body).toHaveProperty('token');
+    });
+  
+    it('should successfully login a user', async () => {
+      const username = 'janedoe';
+      const email = 'janedoe@example.com';
+      const password = 'password456';
+  
+      await prisma.user.create({
+        data: { username, email, password: await bcrypt.hash(password, SALT_COUNT) },
+      });
+  
+      const response = await request(serverInstance)
+        .post('/auth/login')
+        .send({ username, password });
+  
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('user');
+      expect(response.body.user.username).toBe(username);
+      expect(response.body.user.email).toBe(email);
+      expect(response.body).toHaveProperty('token');
+  
+      userToken = response.body.token;
+    });
+  
+    it('should successfully retrieve user profile with valid token', async () => {
+      const response = await request(serverInstance)
+        .get('/auth/account')
+        .set('Authorization', `Bearer ${userToken}`);
+  
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('username');
+      expect(response.body).toHaveProperty('email');
+    });
+  });
+  
--- server/__tests__/auth.test.js
+++ server/__tests__/auth.test.js
@@ -1,62 +1,119 @@
-// const request = require("supertest");
+const app = require('../app');
+const request = require('supertest');
+const userData = require('../auth/index');
 
-// const app = require('../app');
 
-// const prismaMock = require('../../mocks/prismaMock');
+// <------------ ADMIN TESTING ------------> 
 
-// const bcrypt = require('bcrypt');
-// jest.mock('bcrypt');
 
-// const jwt = require('jsonwebtoken');
-// jest.mock('jsonwebtoken');
+describe('GET /auth/users', () => {
+    it('should return all users for admin user', async () => {
+        const username = 'muabria';
+        const email = 'email@email.com';
+        const password = 'F@ncySh0es';
+        const response = await get('/auth/users', 
+        {
+            username,
+            email,
+            password,
+            isAdmin: true
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('user');
+        expect(response.body.user).toHaveProperty('username', username);
+        expect(response.body.user).toHaveProperty('email', email);
+        expect(response.body).toHaveProperty('token');
+        const user = await prisma.user.findUnique({ where: { username } });
+        expect(user).not.toBeNull();
+    });
 
-// describe('/auth', () => {
-//     beforeEach(() => {
-//         jest.resetAllMocks();
-//         jwt.sign.mockReset();
-//         jwt.verify.mockReset();
-//         bcrypt.hash.mockReset();
-//         bcrypt.compare.mockReset();
-    
-//     });
-//     describe('GET /auth', () => {
-//         it('returns list of all users', async () => { //needs to be an admin 
-//             const users = [
-//                 { id: 1, username: '', name: '', password: 'password', admin: true  },
-//                 { id: 2, username: '', name: '', password: 'password', admin: true  },
-//                 { id: 3, username: '', name: '', password: 'password', admin: true  },
-//                 { id: 4, username: '', name: '', password: 'password', admin: true  },
+    it('should return 401 for non-admin users', async () => {
+        const user = await createUser();
+        const token = generateToken(user);
+        const response = await get('/auth/users', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        expect(response.statusCode).toBe(401);
+    });
+});
 
-//             ];
+// <------------ REGISTER TESTING ------------> 
 
-//             const token = "";
-//             jwt.verify.mockReturnValue(users[0]);
-//             prismaMock.user.findUnique.mockResolvedValue(users[0]);
+describe('POST /auth/register', () => {
+    it('should create a new user and return user and token', async () => {
+        const username = 'testuser';
+        const email = 'testuser@example.com';
+        const password = 'password123';
+        const response = await post('/auth/register', {
+            username,
+            email,
+            password,
+        });
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toHaveProperty('user');
+        expect(response.body.user).toHaveProperty('username', username);
+        expect(response.body.user).toHaveProperty('email', email);
+        expect(response.body).toHaveProperty('token');
+        const user = await prisma.user.findUnique({ where: { username } });
+        expect(user).not.toBeNull();
+    });
 
-//             prismaMock.user.findMany.mockResolvedValue(users);
+    it('should return 400 for missing required fields', async () => {
+        const response = await post('/auth/register');
+        expect(response.statusCode).toBe(400);
+    });
+});
 
-//             const response = await request(app).get('/auth').set('Authorization', 'Bearer '+ token);
-       
-//             expect(response.body).toEqual(users);
-//             expect(response.body[0]).toEqual(users[0]);
-//             expect(response.body[1]).toEqual(users[1]);
-//         });
-//     });
+// <------------ LOGIN TESTING  ------------> 
 
-//     describe('GET /auth/me', () => { // needs to be logged in
-//         it('returns the currently logged in user', async () => {
-//             const loggedInUser = {
-//                 id: 2,
-//                 username: "",
-//                 password: ""
-//             }
-            
-//             const token = "";
-//             jwt.verify.mockReturnValue(loggedInUser);
-//             prismaMock.user.findUnique.mockResolvedValue(loggedInUser);
-            
-//             const response = await request(app).get('/auth/me').send(loggedInUser).set('Authorization', 'Bearer '+ token);
+describe('POST /auth/login', () => {
+    it('should return user and token for valid credentials', async () => {
+        const user = await createUser();
+        const response = await post('/auth/login', {
+            username: user.username,
+            password: user.password,
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('user');
+        expect(response.body.user).toHaveProperty('username', user.username);
+        expect(response.body).toHaveProperty('token');
+    });
 
-//             expect(response.body).toEqual(loggedInUser);
-//         })
-//     })});
+    it('should return 401 for invalid username', async () => {
+        const response = await post('/auth/login', {
+            username: 'nonexistentuser',
+            password: 'password123',
+        });
+        expect(response.statusCode).toBe(401);
+    });
+
+    it('should return 401 for invalid password', async () => {
+        const user = await createUser();
+        const response = await post('/auth/login', {
+            username: user.username,
+            password: 'wrongpassword',
+        });
+        expect(response.statusCode).toBe(401);
+    });
+});
+
+
+// <------------ USER PROFILE TESTING ------------> 
+
+describe('GET /auth/account', () => {
+    it('should return user profile for authenticated user', async () => {
+        const user = await createUser();
+        const token = generateToken(user);
+        const response = await get('/auth/account', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('')
+    })
+});
+
+
