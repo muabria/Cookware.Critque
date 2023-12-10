8c19d573db5c291b70672bcd0885a02f150c25f1
Kat Christensen <katchristensen336@gmail.com>
2023-12-10 12:01:01 -0500

    Fixed search bar in mobile (TO DO: fix AllPost mobile format for slide show

--- client/components/Posts/AllPosts.jsx
+++ client/components/Posts/AllPosts.jsx
@@ -3,13 +3,9 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import Button from '@mui/material/Button';
+import { useMediaQuery, useTheme } from "@mui/material";
 
 import { motion } from 'framer-motion';
 
@@ -22,6 +18,9 @@ import LoadingMessage from '../ErrorMessages/LoadingMessage';
 import SlideShow from '../SlideShow';
 
 const AllPosts = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
     const { data, error, isLoading } = useGetReviewsQuery()
 
     if (isLoading) {
@@ -41,54 +40,49 @@ const AllPosts = () => {
                     Explore Trusted Critiques Made from Real Users
                 </Typography>
             </Box>
-            <Box sx={{ mx: 10, mb: 1 }}>
-                <SearchBar />
-            </Box>
-            <Box sx={{ mx: 5 }}>
-
-                <SlideShow
-                    content={
-                        <>
-                            {data && data.map((review) => (
-                                <Grid container key={review.id} >
-                                    <Grid item xs={8}>
+            {isMobile
+                ?
+                <div>
+                    <Box sx={{ mx: 1, mb: 1 }}>
+                        <SearchBar />
+                    </Box>
+                </div>
+                ://is NOT mobile...
+                <div>
+                    <Box sx={{ mx: 10, mb: 1 }}>
+                        <SearchBar />
+                    </Box>
+                </div>}
+            {isMobile
+                ?
+                <div>
+                    <Box sx={{ mx: 2 }}>
+                        <SlideShow
+                            content={
+                                <>
+                                    {data && data.map((review) => (
                                         <Card key={review.id}
                                             sx={{
                                                 boxShadow: 3,
                                                 backgroundColor: "#F9FBE7",
                                                 border: "solid #D29D2B 2px",
                                                 borderRadius: "10px",
-                                                minWidth: 300,
-                                                minHeight: 300,
-                                                m: 5,
+                                                minWidth: 200,
+                                                maxHeight: 300,
+                                                m: 1,
                                             }}>
                                             <CardHeader sx={{ textAlign: "center", color: "#205375", backgroundColor: "#EACD65" }}
-                                                title={review.title}
-                                            // subheader="TO DO"
-                                            />
-                                            {/* <CardMedia
-                                                component=" "
-                                                height="194"
-                                                image=""
-                                                alt="Equipment Picture"
-                                            /> */}
+                                                title={review.title} />
                                             <CardContent>
                                                 <Typography variant="h6" sx={{ color: "#205375" }}>
                                                     {review.content}
                                                 </Typography>
                                             </CardContent>
-                                            <CardActions disableSpacing>
-                                                {/* <IconButton aria-label="add to favorites">
-                                                    <FavoriteIcon />
-                                                </IconButton>
-                                                <IconButton aria-label="share">
-                                                    <ShareIcon />
-                                                </IconButton> */}
+                                            <CardActions>
                                                 <Link to={`/review/${review.id}`} >
                                                     <Button
                                                         sx={{
-                                                            my: 2,
-                                                            ml: 8,
+                                                            my: 1,
                                                             boxShadow: 3,
                                                             color: "#3C1B1F",
                                                             backgroundColor: "#EACD65",
@@ -99,14 +93,58 @@ const AllPosts = () => {
                                                 </Link>
                                             </CardActions>
                                         </Card>
-                                    </Grid>
-                                </Grid>
-                            ))
+                                    ))
+                                    }
+                                </>
                             }
-                        </>
-                    }
-                />
-            </Box>
+                        />
+                    </Box>
+                </div>
+                :// is NOT mobile...
+                <div>
+                    <SlideShow
+                        content={
+                            <>
+                                {data && data.map((review) => (
+                                    <Card key={review.id}
+                                        sx={{
+                                            boxShadow: 3,
+                                            backgroundColor: "#F9FBE7",
+                                            border: "solid #D29D2B 2px",
+                                            borderRadius: "10px",
+                                            minWidth: 300,
+                                            minHeight: 300,
+                                            m: 5,
+                                        }}>
+                                        <CardHeader sx={{ textAlign: "center", color: "#205375", backgroundColor: "#EACD65" }}
+                                            title={review.title} />
+                                        <CardContent>
+                                            <Typography variant="h6" sx={{ color: "#205375" }}>
+                                                {review.content}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Link to={`/review/${review.id}`} >
+                                                <Button
+                                                    sx={{
+                                                        my: 2,
+                                                        ml: 8,
+                                                        boxShadow: 3,
+                                                        color: "#3C1B1F",
+                                                        backgroundColor: "#EACD65",
+                                                        border: "solid #D29D2B 2px"
+                                                    }}>
+                                                    See the Full Review
+                                                </Button>
+                                            </Link>
+                                        </CardActions>
+                                    </Card>
+                                ))
+                                }
+                            </>
+                        }
+                    />
+                </div>}
         </motion.div>
     );
 }
