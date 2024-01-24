37807aa758b0fef59fe1836e8084da8d65036799
Kat Christensen <katchristensen336@gmail.com>
2024-01-23 21:01:23 -0500

    Breaking hompage into mobile and desktop components

---
+++ client/components/HomePage/Desktop/DesktopHomePage.jsx
@@ -0,0 +1,68 @@
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+import { Link } from "react-router-dom";
+
+import { motion } from "framer-motion";
+
+import MapCategories from "../MapCategories";
+import background from "../kitchenEquipmentBackground.png"
+const DesktopHomePage = () => {
+    return (
+        <div>
+            <Typography variant="h3" sx={{ color: "#205375", textAlign: "center", m: 1 }}>
+                Welcome to Cookware Critique
+            </Typography>
+            <Typography variant="h5" sx={{ color: "#205375", textAlign: "center", m: 1 }}>
+                Home to honest critiques for all types of kitchen equipment from food prep to clean up.
+            </Typography>
+            <div style={{
+                backgroundImage: `url(${background})`,
+                backgroundSize: "contain",
+                height: "700px",
+                backgroundRepeat: "no-repeat"
+            }}>
+                <Box sx={{ minHeight: 900, my: 2 }}>
+                    <Grid container>
+                        <Grid item xs={1}>
+                        </Grid>
+                        <Grid item xs={11} sx={{ mb: 25 }}>
+                            <Stack direction="row">
+                                <MapCategories />
+                            </Stack>
+                        </Grid>
+                    </Grid>
+                    <Grid container>
+                        <Grid item xs={5.5}>
+                        </Grid>
+                        <Grid item xs={6.5}>
+                            <Stack direction="row">
+                                <motion.div whileHover={{ scale: 1.3 }}>
+                                    <Link
+                                        to="/explore"
+                                        style={{ textDecoration: "none" }}>
+                                        <Box sx={{
+                                            boxShadow: 3,
+                                            color: "#FAF3F0",
+                                            backgroundColor: "#D988B9",
+                                            border: "solid #B0578D 2px",
+                                            borderRadius: "100px",
+                                            p: 3
+                                        }}>
+                                            <Typography variant="h6">
+                                                Equipment and Reviews
+                                            </Typography>
+                                        </Box>
+                                    </Link>
+                                </motion.div>
+                            </Stack>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </div>
+        </div>
+    )
+}
+export default DesktopHomePage
--- client/components/HomePage/MapCategories.jsx
+++ client/components/HomePage/MapCategories.jsx
@@ -1,7 +1,5 @@
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card"
 import Typography from "@mui/material/Typography";
 import { useMediaQuery, useTheme } from "@mui/material";
 
@@ -9,6 +7,8 @@ import { Link } from "react-router-dom";
 
 import { motion } from "framer-motion";
 import { useGetCategoriesQuery } from "../../redux/api"
+import MobileMapCategories from "./Mobile/MobileMapCategories";
+import DesktopMapCategories from "./Desktop/DesktopMapCategories";
 
 const MapCategories = () => {
 
@@ -25,50 +25,11 @@ const MapCategories = () => {
         <>
             {isMobile ?
                 <>
-                    {data && data.map((category) => (
-                        <Button>
-                            <motion.div whileHover={{ scale: 1.3 }}>
-                                <Link to={`/category/${category.id}`}
-                                    style={{ textDecoration: "none" }}>
-                                    <Card sx={{
-                                        boxShadow: 3,
-                                        color: "#3C1B1F",
-                                        backgroundColor: "#E7B10A",
-                                        border: "solid #D29D2B 2px" }} >
-                                        <Typography>
-                                            {category.category}
-                                        </Typography>
-                                    </Card>
-                                </Link>
-                            </motion.div>
-                        </Button>
-                    ))
-                    }
+                    <MobileMapCategories />
                 </>
                 : //is NOT mobile...
                 <>
-                    {data && data.map((category) => (
-                        <Box key={category.id} sx={{ m: 2 }}>
-                            <Link to={`/category/${category.id}`}
-                                style={{ textDecoration: "none" }}>
-                                <motion.div whileHover={{ scale: 1.3 }}>
-                                    <Avatar
-                                        sx={{
-                                            boxShadow: 3,
-                                            p: 3,
-                                            color: "#3C1B1F",
-                                            backgroundColor: "#E7B10A",
-                                            border: "solid #D29D2B 2px"
-                                        }}>
-                                        <Typography sx={{ fontSize: "13px" }}>
-                                            {category.category}
-                                        </Typography>
-                                    </Avatar>
-                                </motion.div>
-                            </Link>
-                        </Box>
-                    ))
-                    }
+                  <DesktopMapCategories />
                 </>
             }
 
