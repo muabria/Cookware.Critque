2199c2194df50b2c43ff415790c119aa3a2414d0
Marisa <marisavandellos@gmail.com>
2024-03-12 14:30:03 -0700

    added admin delete comment to redux

---
+++ client/components/Dashboards/AdminDashboard/Desktop/components/DesktopAdminMapComm.jsx
@@ -0,0 +1,24 @@
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid"
+import Rating from "@mui/material/Rating"
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import PreviewIcon from '@mui/icons-material/Preview';
+import DeleteForeverSharpIcon from '@mui/icons-material/DeleteForeverSharp';
+
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useGetCommentsQuery } from "../../../../../redux/api";
+
+const DesktopAdminMapComm = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default DesktopAdminMapComm
